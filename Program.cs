ebade93eb5cfad6dfe56cdade5efe761abf62583
Tim Kerstiens <tim@kerstiens.io>
2024-02-17 20:01:54 -0600

    Added CORS configuration

--- Program.cs
+++ Program.cs
@@ -8,6 +8,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    // Development CORS Policy
+    options.AddPolicy("DevCorsPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Allow specific origin for development
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+
+    // Production CORS Policy
+    options.AddPolicy("ProdCorsPolicy", policy =>
+    {
+        policy.WithOrigins("https://market.catalyst.black") // Use your production domain here
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,6 +34,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    app.UseCors("DevCorsPolicy"); // Use Development CORS Policy
+}
+else
+{
+    app.UseHttpsRedirection();
+    app.UseCors("ProdCorsPolicy"); // Use Production CORS Policy
 }
 
 app.UseHttpsRedirection();
